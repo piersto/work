f202b781fbe41f04ded667c48e6de751556433d6
piersto <44554156+piersto@users.noreply.github.com>
2024-03-01 15:22:03 -0500

    Changed code to take flatten json including all dictionaries

--- test/EOD/test_type_of_fields.py
+++ test/EOD/test_type_of_fields.py
@@ -1,24 +1,26 @@
 import json
-import pytest
 import os
+
+
+def flatten_json(json_dict, parent_key=''):
+    values = {}
+    for key, value in json_dict.items():
+        new_key = parent_key + '_' + key if parent_key else key
+        if isinstance(value, dict):
+            nested_values = flatten_json(value, new_key)
+            values.update(nested_values)
+        elif value is None:
+            values[new_key] = "null"
+        elif isinstance(value, str) and value.lower() in ('true', 'false'):
+            values[new_key] = value.lower() == 'true'
+        else:
+            values[new_key] = value
+    return values
 
 
 def extract_values_from_json(json_file_path):
     with open(json_file_path, 'r') as file:
         data = json.load(file)
-
-    def flatten_json(json_dict):
-        values = {}
-        for key, value in json_dict.items():
-            if isinstance(value, dict):
-                nested_values = flatten_json(value)
-                for nested_key, nested_value in nested_values.items():
-                    values[key + "_" + nested_key] = nested_value
-            elif value is None:
-                values[key] = "null"
-            else:
-                values[key] = value
-        return values
 
     return flatten_json(data)
 
@@ -40,10 +42,10 @@ def test_json_data_types():
     print("Extracted values from JSON:", data_values)
 
     # Define the fields for each type
-    string_fields = ['person_name', 'person_surname']
-    int_fields = ['person_age']
-    float_fields = ['person_height']
-    bool_fields = ['person_is_student', 'person_is_married']
+    string_fields = ['persons_person_name', 'persons_person_surname']
+    int_fields = ['persons_person_age']
+    float_fields = ['persons_person_height']
+    bool_fields = ['persons_person_is_student', 'persons_person_is_married']
 
     # Test string fields
     for field in string_fields:
@@ -64,5 +66,6 @@ def test_json_data_types():
     for field in bool_fields:
         value = data_values.get(field)
         assert value is None or isinstance(value, bool), f"Field '{field}' is not of type bool or null. Actual type: {type(value)}."
-
-
+        assert value is not isinstance(value, int), f"Field '{field}' is not of type bool or null. Actual type: {type(value)}."
+        assert value is not isinstance(value, str), f"Field '{field}' is not of type bool or null. Actual type: {type(value)}."
+        assert value is not isinstance(value, float), f"Field '{field}' is not of type bool or null. Actual type: {type(value)}."
